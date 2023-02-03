fac5085437ac7e1b7db14b1ea8143c8a2cd0094b
Sohaib Manah <souhaibemanah@gmail.com>
2023-02-03 13:09:08 +0100

    adding posts

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,86 +1,50 @@
-import { ClassNames } from '@emotion/react';
-import axios from 'axios';
-import React, { FunctionComponent, useEffect, useState } from 'react';
+// import { ClassNames } from "@emotion/react";
+import axios, { AxiosResponse } from "axios";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { Post } from "../Interfaces/Interfaces";
+import Card from "../components/Card/Card";
+import "../css/main.scss";
 
+interface HomeProps {}
 
-interface HomeProps {
-    
-    
-    
-}
- 
 const Home: FunctionComponent<HomeProps> = () => {
+  const [posts, getPosts] = useState<Post[]>([]);
+  //   const [counter, setCounter] = useState(0);
 
-    const [posts,getPosts] = useState<any[]>([]);
-    const [counter,setCounter] = useState(0);
+  useEffect(() => {
+    axios
+      .get(axios.defaults.baseURL + "posts?n=10")
+      .then((response: AxiosResponse) => {
+        // console.log("🚀 ~ file: Home.tsx:31 ~ .then ~ response", response);
+        const posts = response.data.data as [Post];
+        // console.log("🚀 ~ file: Home.tsx:32 ~ .then ~ posts", posts);
+        getPosts(posts);
+      })
 
-    useEffect(() => {
-        getRandomPosts();
-   }, []);
+      .catch((error) => console.error(error));
+  }, []);
 
+  //   let commentsCounter = (postId: string) => {
+  //     axios
+  //       .get(axios.defaults.baseURL + `comments/${postId}`)
+  //       .then((response) => {
+  //         setCounter(response.data.data.length);
+  //       })
 
+  //       .catch((error) => console.error(error));
+  //   };
 
-    let getRandomPosts=()=>{
-        
-        axios.get(axios.defaults.baseURL+"posts?n=10").then((response)=>{
+  return (
+    <div className="posts">
+      {posts && posts.map((post) => <Card key={post._id} {...post} />)}
+    </div>
+  );
+};
 
-                getPosts(response.data)
-
-
-        })
-        
-        .catch(error => console.error(error));
-    
-
-    }
-
-    let commentsCounter=(postId:string)=>{
-
-        axios.get(axios.defaults.baseURL+`comments/${postId}`).then((response)=>{
-
-            setCounter(response.data.data.length);
-            
-
-    })
-    
-    .catch(error => console.error(error));
-
-
-
-    }
-
-    return ( <div>
-        <h1>Posts</h1>
-        <ul className='posts'>
-
-        {
-            posts && posts.map((post)=>{
-
-                {commentsCounter(post._id)}
-
-                return(
-                        <li className='post'>
-                            <h2> {post.title}({post._id})</h2>
-                            <hr />
-                            <p> {post.body}</p>
-                            <hr />
-                            <span>
-                                <a href="#">Comments {counter}</a>
-                            </span> 
-                            <br />
-                            <br />
-                        </li>
-                )
-
-            
-            })
-        }
-    
-        </ul>
-
-    </div> );
-}
- 
 export default Home;
 
+// {
 
+//   commentsCounter(post._id);
+// }
+//   console.log(posts);
